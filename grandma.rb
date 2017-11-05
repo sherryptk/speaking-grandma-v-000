f566d13bf876d1750cf81644f06a41a4444fe3a7
Sherry <sherryptk@gmail.com>
2017-11-05 16:53:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string.upcase
-    puts "NO, NOT SINCE 1938!"
+    NO, NOT SINCE 1938
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
