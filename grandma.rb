3307d2c49de745a26082b2c1230df021a6325247
Sherry <sherryptk@gmail.com>
2017-11-05 16:54:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(string)
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
 
   end
 end
